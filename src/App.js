11de62220a391a88e8c8133128e051fe9ff50328
Yoga Hendrapratama <yhepra@gmail.com>
2022-07-03 12:46:01 +0700

    trial components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
+import { Hello } from "./components/Hello";
 import { Welcome } from "./components/Welcome";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Greet />
       <Welcome />
+      <Hello />
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,17 @@
+import React, { createElement } from "react";
+
+export const Hello = () => {
+  // Menggunakan JSX
+  return (
+    <div>
+      <h1>Hello JSX</h1>
+    </div>
+  );
+
+  // Menggunakan JS Biasa
+  //   return React.createElement(
+  //     "div",
+  //     null,
+  //     createElement("h1", null, "Hello JavaScript")
+  //   );
+};
